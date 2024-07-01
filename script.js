6f6ce4ac221657a7b0432d20092aff7bd17f0b30
karaqme <rigovsema@mail.ru>
2024-07-01 15:26:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let balance = parseInt(document.querySelector('.balance').textContent.replace(/[^0-9]/g, ''));
-function addBalance() {
-    balance++;
-    document.querySelector('.balance').innerHTML = '<img src="msg6362875622-76286 (1).png" alt="" class="img1"> ' + balance;
+function addBalance(event) {
+    if (event.touches) {
+        event.preventDefault();
+        for (let i = 0; i < event.touches.length; i++) {
+            let touch = event.touches[i];
+            if (touch.target.classList.contains('img2')) {
+                balance++;
+                document.querySelector('.balance').innerHTML = '<img src="msg6362875622-76286 (1).png" alt="" class="img1"> ' + balance;
+            }
+        }
+    }
 }
