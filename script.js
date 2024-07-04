457e6f0844d2d8f46cccbfbf920875cb141c251b
karaqme <rigovsema@mail.ru>
2024-07-04 16:21:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ function addBalance() {
 let home1 = document.getElementById('home1')
 let home2 = document.getElementById('home2')
 let home3 = document.getElementById('home3')
+let div1 = document.getElementById('div1')
+let div2 = document.getElementById('div2')
+let div3 = document.getElementById('div3')
 
 function malou2() {
     home1.style.display = 'none';
     home3.style.display = 'none';
     home2.style.display = 'block';
+    div1.style.background = 'black';
+    div3.style.background = 'black';
+    div2.style.background = 'rgb(36, 36, 48)';
 }
 function malou1() {
     home2.style.display = 'none';
     home3.style.display = 'none';
     home1.style.display = 'block';
+    div1.style.background = 'rgb(36, 36, 48)';
+    div3.style.background = 'black';
+    div2.style.background = 'black';
 }
 function malou3() {
     home1.style.display = 'none';
     home2.style.display = 'none';
     home3.style.display = 'block';
+    div1.style.background = 'black';
+    div3.style.background = 'rgb(36, 36, 48)';
+    div2.style.background = 'black';
 }
