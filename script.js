eef3dd5fc140582480ba46d322d20a0f79cd4634
karaqme <rigovsema@mail.ru>
2024-07-15 16:35:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let tg = window.Telegram;
 if (tg != undefined) {
   if (tg.WebApp != undefined && tg.WebApp.initData != undefined) {
     let safe = tg.WebApp.initData;
-    tg.isVerticalSwipesEnabled = false;
     tg.WebApp.backgroundColor = '#000';
     tg.WebApp.headerColor = '#000';
     tg.WebApp.expand();
+    window.Telegram.WebApp.close()
   }
 }
 
