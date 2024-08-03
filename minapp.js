21f442ad6678296853964203dc667f8b8216c899
karaqme <rigovsema@mail.ru>
2024-08-03 12:28:47 +0300

    Add files via upload

--- minapp.js
+++ minapp.js
@@ -2,9 +2,10 @@ let tg = window.Telegram;
 if (tg != undefined) {
   if (tg.WebApp != undefined && tg.WebApp.initData != undefined) {
     let safe = tg.WebApp.initData;
+    tg.WebApp.ready()
     tg.WebApp.backgroundColor = '#000';
     tg.WebApp.headerColor = '#000';
     tg.WebApp.expand();
-    tg.WebApp.enableClosingConfirmation();
+    tg.WebApp.disableVerticalSwipes();
   }
 }
